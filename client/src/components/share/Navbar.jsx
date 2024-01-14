57f0deb2c18eed60d1f2aa10b74427c2948ef90f
faezeh <faezeh.kazemzadeh@gmail.com>
2024-01-14 22:11:18 +0100

    add two more routes to navbar

--- client/src/components/share/Navbar.jsx
+++ client/src/components/share/Navbar.jsx
@@ -1,55 +1,63 @@
-import React from 'react'
-import {Routes , Route, Navigate} from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import {Header,Layout, PrivateRoute , UnAuthorized} from '../index'
-import {Home , SignIn,SignUp,About,Profile} from '../../pages/index';
-import {AllProducts,RemoveProduct,AddProduct,Dashboard, UpdateProduct} from '../admin/index' ;
+import { Header, Layout, PrivateRoute, UnAuthorized } from "../index";
+import { Home, SignIn, SignUp, About, Profile } from "../../pages/index";
+import {
+  AllProducts,
+  RemoveProduct,
+  AddProduct,
+  Dashboard,
+  UpdateProduct,
+} from "../admin/index";
 
 const ROLES = {
-    Admin: "Admin",
-    Editor: "Editor",
-    User: "User",
-  };
+  Admin: "Admin",
+  Editor: "Editor",
+  User: "User",
+};
 export default function Navbar() {
-    const { currentUser } = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <>
-              <Header />
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route path="/" element={<Home />} />
-              <Route
-                element={
-                  <PrivateRoute allowedRoles={[ROLES.User, ROLES.Admin]} />
-                }
-              >
-                <Route path="profile" element={<Profile />} />
-              </Route>
-              <Route
-                path="sign-in"
-                element={
-                  currentUser ? <Navigate to="/" replace={true} /> : <SignIn />
-                }
-              />
-              <Route
-                path="sign-up"
-                element={
-                  currentUser ? <Navigate to="/" replace={true} /> : <SignUp />
-                }
-              />
-              <Route path="about" element={<About />} />
-              <Route element={<PrivateRoute allowedRoles={[ROLES.Admin]} />}>
-                <Route path="admin/dashboard/*" element={<Dashboard />}>
-                  <Route path="product/add" element={<AddProduct />} />
-                  <Route path="product/update/:id" element={<UpdateProduct />} />
-                  <Route path="product/delete" element={<RemoveProduct />} />
-                  <Route path="product/getAll" element={<AllProducts />} />
-                </Route>
-              </Route>
-              <Route path="/unauthorized" element={<UnAuthorized />} />
-              <Route path="/*" element={<UnAuthorized />} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route
+            element={<PrivateRoute allowedRoles={[ROLES.User, ROLES.Admin]} />}
+          >
+            <Route path="profile" element={<Profile />} />
+          </Route>
+          <Route
+            path="sign-in"
+            element={
+              currentUser ? <Navigate to="/" replace={true} /> : <SignIn />
+            }
+          />
+          <Route
+            path="sign-up"
+            element={
+              currentUser ? <Navigate to="/" replace={true} /> : <SignUp />
+            }
+          />
+          <Route path="about" element={<About />} />
+          <Route element={<PrivateRoute allowedRoles={[ROLES.Admin]} />}>
+            <Route path="admin/dashboard/*" element={<Dashboard />}>
+            <Route index element={<AddProduct />} />
+              <Route path="product/add" element={<AddProduct />} />
+              <Route path="product/update/:id" element={<UpdateProduct />} />
+              <Route path="product/delete" element={<RemoveProduct />} />
+              <Route path="product/getAll" element={<AllProducts />} />
             </Route>
-          </Routes></>
-  )
+          </Route>
+          <Route path="/about" element={<About />} />
+
+          <Route path="/unauthorized" element={<UnAuthorized />} />
+          <Route path="/*" element={<UnAuthorized />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }

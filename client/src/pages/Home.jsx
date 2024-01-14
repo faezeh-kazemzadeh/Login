28ed2655e2d2f32f2625873215f70ffecc08de10
faezeh <faezeh.kazemzadeh@gmail.com>
2024-01-14 22:12:29 +0100

    add firs structure of ome page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,17 @@
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <section className="w-100 ">
+        <div>Hero</div>
+      </section>
+      <section>
+        <div>products</div>
+      </section>
+      <section>
+        <div>most Popular</div>
+      </section>
+    </div>
   )
 }

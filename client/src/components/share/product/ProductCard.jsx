cd0e95096e9e7160e6e08c4d47e5968763ccd324
faezeh <faezeh.kazemzadeh@gmail.com>
2024-01-16 23:57:53 +0100

    product Swiper in home page

---
+++ client/src/components/share/product/ProductCard.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import styles from "./product.module.css";
+import { truncate } from "../../../utils/string";
+export default function ProductCard({ product }) {
+  const [discountPrice, setDiscountPrice] = useState(0);
+  useEffect(() => {
+    const discountAmount = (product.regularPrice * product.discount) / 100;
+    const calculatedDiscountPrice = product.regularPrice - discountAmount;
+    setDiscountPrice(calculatedDiscountPrice);
+  }, [product]);
+  return (
+    <div className={styles.productCard}>
+      <div className={styles.productImageBox}>
+        <img
+          src={`/images/${product.imageUrls[0].name}`}
+          srcSet={`/images/${product.imageUrls[0].name}`}
+          alt={product.name}
+        />
+      </div>
+      <div className={styles.productContent }>
+        <header className={styles.productName}>{truncate(product.name,20)}</header>
+        <p className={styles.productDiscountedPrice}>
+          {product.discount>0 ? discountPrice : product.regularPrice}
+        </p>
+        <p className={styles.price}>
+         <span>Originally:</span> <span className={styles.productPrice}>{ product.regularPrice}</span>
+        </p>
+        <span className={styles.productDiscount}>
+          {product.discount>0 && `${product.discount}%`}
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/share/product/ProductSwiper.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchProducts } from "../../../redux/product/productsSlice";
+
+import ProductCard from "./ProductCard";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default function ProductSwiper() {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  const breakpoints = {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    480: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  };
+  return (
+    <>
+      {products && products.length > 0 ? (
+        <Swiper
+        breakpoints={breakpoints}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={false}
+          modules={[ Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {products.map((product) => (
+            <SwiperSlide key={product._id}>
+              <ProductCard product={product} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <p>nist</p>
+      )}
+    </>
+  );
+}

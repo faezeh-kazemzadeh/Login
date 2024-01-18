156644cbd5fdefdb3b46e2693704eeed65f862ed
faezeh <faezeh.kazemzadeh@gmail.com>
2024-01-18 18:51:38 +0100

    update Product select

--- client/src/components/admin/product/UpdateProduct.jsx
+++ client/src/components/admin/product/UpdateProduct.jsx
@@ -118,7 +118,7 @@ export default function UpdateProduct() {
               onChange={changeHandler}
               className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-                        <option value="hair care">Hair care</option>
+                        <option value="Hair care">Hair care</option>
             <option value="Skin care">Skin care</option>
             <option value="Make-up">Make-up</option>
             </select>

626d91aeea5ce46a22ef369bb6f66600c683af48
faezeh <faezeh.kazemzadeh@gmail.com>
2024-01-23 18:33:06 +0100

    products detail page styles

---
+++ client/src/components/share/product/ProductDetail.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams , useNavigate} from "react-router-dom";
+import { truncate } from "../../../utils/string";
+import { fetchProducts } from "../../../redux/product/productsSlice";
+import { TbBasketPlus } from "react-icons/tb";
+import { IoTrashOutline } from "react-icons/io5";
+import { IoIosArrowBack } from "react-icons/io";
+import styles from "./productDetail.module.css";
+export default function ProductDetail() {
+  const params = useParams();
+  const navigate=useNavigate()
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+  const [product, setProduct] = useState(undefined);
+  const [discountPrice, setDiscountPrice] = useState(0);
+  const [productImage, setProductImage] = useState(undefined);
+  useEffect(() => {
+    if (products.length === 0) {
+      dispatch(fetchProducts());
+    }
+  }, []);
+  useEffect(() => {
+    const foundProduct = products.find((product) => product._id === params.id);
+    setProduct(foundProduct);
+  }, [products]);
+  useEffect(() => {
+    if (product) {
+      const discountAmount = (product.regularPrice * product.discount) / 100;
+      const calculatedDiscountPrice = product.regularPrice - discountAmount;
+      setDiscountPrice(calculatedDiscountPrice);
+      setProductImage(`/images/${product.imageUrls[0].name}`);
+    }
+  }, [product]);
+
+  return (
+    product && (
+      <div className={styles.productCard}>
+        <div className={styles.productImageBox}>
+          {product.imageUrls.length > 1 && (
+            <div className={styles.images}>
+              {product.imageUrls.map((image, imdex) => {
+                return (
+                  <img
+                    key={imdex}
+                    src={`/images/${image.name}`}
+                    onClick={() => setProductImage(`/images/${image.name}`)}
+                    alt={truncate(image.name)}
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                );
+              })}
+            </div>
+          )}
+          <div className={styles.thumbnail}>
+            <img src={productImage} srcSet={productImage} alt={product.name} />
+          </div>
+        </div>
+        <div className={styles.productContent}>
+         <article>
+         <h1 className={styles.productName}>
+            {truncate(product.name, 25)}
+          </h1>
+          <p>{product.description}</p>
+          
+            {product.discount > 0 ? <p className={styles.productDiscountedPrice}><span>Discounted Price : </span>{discountPrice} </p> :''}
+         
+          <p className={styles.price}>
+            <span>Original Price :</span>{" "}
+            <span className={styles.productPrice}>{product.regularPrice}</span>{" "}
+            <span className={styles.productDiscount}>
+              {product.discount > 0 && `-${product.discount}%`}
+            </span>
+          </p>
+          </article>
+        <div className="flex justify-between">
+        <div className={styles.actions}>
+            <button type="button" className={styles.add}>
+              <TbBasketPlus />
+            </button>
+            <button type="button"><IoTrashOutline/></button>
+            <button type="button">+</button>
+            <button type="button">-</button>
+          </div>
+          <button type="button" onClick={()=> navigate(-1)} className="flex items-center"><IoIosArrowBack /> Back</button>
+        </div>
+        </div>
+      </div>
+    )
+  );
+}

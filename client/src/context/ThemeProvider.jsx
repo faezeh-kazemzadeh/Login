0e2342712ee35ff3179cde4746d7cb65ab2db52c
faezeh <faezeh.kazemzadeh@gmail.com>
2024-01-25 11:55:44 +0100

    change handleClick function

--- client/src/context/ThemeProvider.jsx
+++ client/src/context/ThemeProvider.jsx
@@ -1,33 +1,39 @@
-import React, {  createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from "react";
 
-export const ThemeContext = createContext()
-const initialState={
-    userProfile:false,
-    cart:false
-}
-export default function ThemeProvider({children}) {
-    const [activeMenu,setActiveMenu]=useState(true)
-    const [isClicked, setisClicked] = useState(initialState)
-    const [screenSize, setScreenSize] = useState(undefined)
-    const handleClick =(clicked)=>{
-        console.log(clicked)
-        setisClicked({...initialState,[clicked]:true})
+export const ThemeContext = createContext();
+const initialState = {
+  userProfile: false,
+  cart: false,
+  notification: false,
+};
+export default function ThemeProvider({ children }) {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const handleClick = (clicked) => {
+    if (isClicked[clicked] === false) {
+      setIsClicked({ ...initialState, [clicked]: true });
+    } else {
+      setIsClicked({ ...initialState, [clicked]: false });
     }
+  };
   return (
-    <ThemeContext.Provider value={{
+    <ThemeContext.Provider
+      value={{
         activeMenu,
         setActiveMenu,
         isClicked,
-        setisClicked,
+        setIsClicked,
         handleClick,
         screenSize,
-        setScreenSize
-    }}>
-        {children}
+        setScreenSize,
+      }}
+    >
+      {children}
     </ThemeContext.Provider>
-  )
+  );
 }
-export const useThemeContext =()=> {
-    const themes= useContext(ThemeContext);
-    return themes
-}
+export const useThemeContext = () => {
+  const themes = useContext(ThemeContext);
+  return themes;
+};
